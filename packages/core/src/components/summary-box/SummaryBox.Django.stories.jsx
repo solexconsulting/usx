90164e6d227cf2a9687aae17910bc514df9b49ba
Alexander Olson <aolsonbiz@gmail.com>
2026-05-16 05:33:24 +0000

    Implemented Summary Box component

--- packages/core/src/components/summary-box/SummaryBox.Django.stories.jsx
+++ packages/core/src/components/summary-box/SummaryBox.Django.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { djangoComponent } from '../../utils/djangoComponent.js';
 import config from './config.json';
-import { buildArgTypes, componentTag } from '../../utils/storyHelpers';
+import { buildArgTypes, createDjangoStory } from '../../utils/storyHelpers';
+import { storyDefs } from './SummaryBox.React.stories.jsx';
 
 const generatedArgTypes = buildArgTypes(config.props || {});
 
@@ -9,16 +9,22 @@ export default {
   title: 'Django/SummaryBox',
   tags: ['autodocs'],
   argTypes: generatedArgTypes,
+  excludeStories: ['djangoStoryDefs'],
 };
 
-export const Default = {
-  args: config.default || {},
-  parameters: {
-    docs: {
-      source: {
-        code: componentTag({ name: 'summary-box', props: config.default || {} })
-      }
-    }
+const createStory = createDjangoStory('summary-box');
+
+export const djangoStoryDefs = {
+  WithListMarkup: {
+    heading: 'Program timeline summary',
+    content: '<ol class="usa-list"><li><strong>Week 1-2:</strong> Established intake baselines and partner staffing map.</li><li><strong>Week 3-6:</strong> Launched weekend processing and two temporary warming sites.</li><li><strong>Week 7-10:</strong> Expanded SMS intake and reduced median placement times.</li><li><strong>Week 11-12:</strong> Prepared permanent staffing and statewide scale recommendations.</li></ol>',
   },
-  render: djangoComponent('summary-box')
 };
+
+export const ExecutiveSummary = createStory(storyDefs.ExecutiveSummary);
+export const StoryStep1Context = createStory(storyDefs.StoryStep1Context);
+export const StoryStep2Actions = createStory(storyDefs.StoryStep2Actions);
+export const StoryStep3Outcomes = createStory(storyDefs.StoryStep3Outcomes);
+export const StoryStep4Decision = createStory(storyDefs.StoryStep4Decision);
+export const ChildrenPriority = createStory(storyDefs.ChildrenPriority);
+export const WithListMarkup = createStory(djangoStoryDefs.WithListMarkup);
--- packages/core/src/components/summary-box/SummaryBox.jsx
+++ packages/core/src/components/summary-box/SummaryBox.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ClassNames from 'classnames';
 import './summary-box.scss';
 
-export default function SummaryBox({ children = null, className = '', ...props }) {
-  const classes = ['usx-summary-box', className].filter(Boolean).join(' ');
+export default function SummaryBox({
+  id = 'summary-box-heading',
+  heading,
+  content = '',
+  children = null,
+  className = '',
+  ...props
+}) {
+  const classes = ClassNames('usa-summary-box', 'usx-summary-box', className);
+  const headingId = id;
+  const bodyContent = children ?? content;
+
   return (
-    <div className={classes} {...props}>
-      {children || 'SummaryBox'}
+    <div
+      className={classes}
+      role="region"
+      aria-labelledby={headingId}
+      {...props}
+    >
+      <div className="usa-summary-box__body">
+        <h4 className="usa-summary-box__heading" id={headingId}>{heading}</h4>
+        <div className="usa-summary-box__text">
+          {bodyContent}
+        </div>
+      </div>
     </div>
   );
 }
 
 SummaryBox.propTypes = {
+  id: PropTypes.string,
+  heading: PropTypes.string.isRequired,
+  content: PropTypes.node,
   children: PropTypes.node,
   className: PropTypes.string,
 };
