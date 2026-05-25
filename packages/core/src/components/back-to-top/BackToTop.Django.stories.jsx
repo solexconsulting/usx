506975c3df9084afddab1147b86562755d3c607e
Alexander Olson <aolsonbiz@gmail.com>
2026-05-25 03:10:13 +0000

    Added back-to-top component

--- packages/core/src/components/back-to-top/BackToTop.Django.stories.jsx
+++ packages/core/src/components/back-to-top/BackToTop.Django.stories.jsx
@@ -10,5 +10,17 @@ export default {
 };
 
 const createStory = createDjangoStory({ componentName: 'back-to-top' });
+const allowedPropNames = new Set(Object.keys(config.props || {}));
+const toDjangoArgs = (args) => Object.fromEntries(
+  Object.entries(args).filter(([key]) => allowedPropNames.has(key))
+);
 
-export const Default = createStory(storyDefs.Default);
+export const Default = createStory(toDjangoArgs(storyDefs.Default));
+export const Primary = createStory(toDjangoArgs(storyDefs.Primary));
+export const IconOnlySmall = createStory(toDjangoArgs(storyDefs.IconOnlySmall));
+export const IconOnlyLarge = createStory(toDjangoArgs(storyDefs.IconOnlyLarge));
+export const LinkDefault = createStory(toDjangoArgs(storyDefs.LinkDefault));
+export const DefaultIconLeft = createStory(toDjangoArgs(storyDefs.DefaultIconLeft));
+export const PrimaryIconLeft = createStory(toDjangoArgs(storyDefs.PrimaryIconLeft));
+export const LinkIconLeft = createStory(toDjangoArgs(storyDefs.LinkIconLeft));
+
